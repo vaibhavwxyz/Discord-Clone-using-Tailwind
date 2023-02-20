1554e096c098c3deb4e2b442eb3599aca0fe7592
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-02-20 13:20:31 +0530

    repo updated

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const menuBtn = document.querySelector("#menuBtn");
 const closeBtn = document.querySelector("#closeBtn");
 
 menuBtn.addEventListener("click", () => {
-	sideMenu.style.width = "288px";
+  sideMenu.style.width = "288px";
 });
 
 closeBtn.addEventListener("click", () => {
-	sideMenu.style.width = "0";
+  sideMenu.style.width = "0";
 });
